aa6e02b1e8eaacf9dfdb5b4273cadbb507f07ca8
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-08-29 10:13:47 -0500

    subiendo calculadora final (creo)

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@ var opciones = parseInt(prompt("escoja una opción: \n" +
 	"2. Resta\n" +
     "3. Multiplicación\n" +
     "4. División\n" +
-    "5. Potencia\n" +
-    "6. Radicación\n" +
-    "7. Mayor\n" +
-	"8. Menor"
+    "5. Residuo o resto\n" +
+    "6. ¿Cuál es menor?\n" +
+    "7. ¿Cuál es mayor?"
     ));
 
 var numero1 = parseInt(prompt("Ingresa tu número 1"));
@@ -14,41 +13,40 @@ var numero2 = parseInt(prompt("Ingresa tu número 2"));
 
 switch(opciones){
 	case 1:
-	var suma = numero1 + numero2;
-	document.write("La suma es:" + suma);
-	break;
-        
+        var suma = numero1 + numero2;
+        document.write("La suma es:" + suma);
+        break;
 	case 2:
-	var resta = numero1 - numero2;
-	document.write("La resta es:" + resta);
+        var resta = numero1 - numero2;
+        document.write("La resta es:" + resta);
         break;
-        
     case 3:
-    var multiplicacion = numero1 * numero2;
-    document.write("La resta es:" + multiplicacion);    
+        var multiplicacion = numero1 * numero2;
+        document.write("La multiplicación es:" + multiplicacion);    
         break;
-        
     case 4:
-    var division = numero1 / numero2;
-    document.write("La resta es:" + division);    
+        var division = numero1 / numero2;
+        document.write("La división es:" + division);    
         break;
-        
     case 5:
-    var residuo = numero1 % numero2;
-    document.write("La resta es:" + residuo);    
+        var residuo = numero1 % numero2;
+        document.write("El residuo es:" + residuo);    
         break;
-        
     case 6: 
-        
-    var potencia = Math.pow(numero1,numero2);
-    document.write("La resta es:" + potencia);    
-    break;
-    
-    case 6:    
-    var  radicacion = Math.sqrt2(numero1);
-         document.write("La raiz cuadrada es " + radicacion);
+        if(numero1 < numero2){
+            document.write("El número menor es " + numero1);
+        } else{
+            document.write("El número menor es " + numero2);   
+        }
         break;
-	default:
-	document.write("no es una opción valida");
-    break;	
+    case 7:    
+        if(numero1 > numero2){
+            document.write("El número mayor es " + numero1);
+        } else{
+            document.write("El número mayor es " + numero2);   
+        }
+        break;   
+    default:
+        document.write("Eso no es válido joven");
+        break;
 }
